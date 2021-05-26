451ff9781a8788e908c1399a4bd9fc0c22907355
Вячеслав <oganesson@bk.ru>
2021-05-26 20:07:03 +0300

    исправлены методы

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,18 +13,13 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     private Connection connection;
-    private Statement statement;
     public UserDaoJDBCImpl() {
         connection = Util.getConnection();
-        try{
-            statement = connection.createStatement();
-        }catch(SQLException e){
-
-        }
     }
 
-    public void createUsersTable()  {
-        try{
+    public void createUsersTable() throws SQLException {
+        try(Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
             String SQL = "CREATE TABLE IF NOT EXISTS users " +
                     "(id BIGINT NOT NULL AUTO_INCREMENT, " +
                     " name VARCHAR(50), " +
@@ -32,58 +27,81 @@ public class UserDaoJDBCImpl implements UserDao {
                     " age TINYINT UNSIGNED not NULL," +
                     "PRIMARY KEY(id));";
             statement.execute(SQL);
-            System.out.println("Table successfully created...");
+            connection.commit();
         } catch(SQLException e){
-            System.out.println("Не удалось создать таблицу");
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
-    public void dropUsersTable() {
-        try{
+    public void dropUsersTable() throws SQLException{
+        try(Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
             statement.execute("DROP TABLE IF EXISTS users");
+            connection.commit();
         } catch(SQLException e){
-            System.out.println("Ошибка при возврате списка юзеров");
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-        try{
+    public void saveUser(String name, String lastName, byte age) throws SQLException{
+        try(Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
             statement.executeUpdate("INSERT INTO users(name, lastName, age) " +
                     "VALUES('" + name + "', '" + lastName + "', " + age + ");");
             System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+            connection.commit();
         }catch(SQLException e){
-            System.out.println("Ошибка при добавлении юзера в таблицу");
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
-    public void removeUserById(long id) {
-        try{
-            statement.executeUpdate("DELETE FROM users WHERE id =" + id + ")");
+    public void removeUserById(long id) throws SQLException{
+        try(Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
+            statement.executeUpdate("DELETE FROM users WHERE id =" + id);
+            connection.commit();
         }catch(SQLException e){
-            System.out.println("Ошибка при удалении юзера");
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
     public List<User> getAllUsers() {
         ResultSet users;
         List<User> userList = new ArrayList<>();
-        try{
+        try(Statement statement = connection.createStatement()){
             users = statement.executeQuery("SELECT * FROM users");
             while (users.next()){
                 userList.add(new User(users.getString(2),
                         users.getString(3), users.getByte(4)));
             }
         } catch(SQLException e){
-            System.out.println("Ошибка при возврате списка юзеров");
+            e.printStackTrace();
         }
         return userList;
     }
 
-    public void cleanUsersTable() {
-        try{
+    public void cleanUsersTable() throws SQLException{
+        try(Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
             statement.executeUpdate("DELETE FROM users");
+            connection.commit();
         }catch(SQLException e){
-            System.out.println("Ошибка во время очистки таблицы");
+            e.printStackTrace();
+            connection.rollback();
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,24 +3,41 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-    public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+    public void createUsersTable()  {
+        try{
+            userDaoJDBC.createUsersTable();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+        try{
+            userDaoJDBC.dropUsersTable();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name, lastName, age);
+        try{
+            userDaoJDBC.saveUser(name, lastName, age);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        try{
+            userDaoJDBC.removeUserById(id);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
@@ -28,6 +45,10 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        try {
+            userDaoJDBC.cleanUsersTable();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
