a88da42e9727c5fbf53ec2df2d73aa9849cc3f75
Вячеслав <oganesson@bk.ru>
2021-05-27 23:07:29 +0300

    закомментировал код

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Calvin", "Gibson", (byte)30);
-        userService.saveUser("Milton", "Lloyd", (byte)25);
-        userService.saveUser("Robyn", "Little", (byte)27);
-        userService.saveUser("Darlene", "Heath", (byte)32);
-        List<User> users = userService.getAllUsers();
-        for (User user :
-                users) {
-            System.out.println(user);
-        }
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        UserServiceImpl userService = new UserServiceImpl();
+//        userService.createUsersTable();
+//        userService.saveUser("Calvin", "Gibson", (byte)30);
+//        userService.saveUser("Milton", "Lloyd", (byte)25);
+//        userService.saveUser("Robyn", "Little", (byte)27);
+//        userService.saveUser("Darlene", "Heath", (byte)32);
+//        List<User> users = userService.getAllUsers();
+//        for (User user :
+//                users) {
+//            System.out.println(user);
+//        }
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
     }
 }
